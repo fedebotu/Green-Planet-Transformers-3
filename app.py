e651ada4db8853d357b8b42c5bb023673e384942
skhatter <sumedhakhatter482@gmail.com>
2022-11-13 14:48:07 -0800

    handling edge cases

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from pathlib import Path
-import requests
 
-import streamlit as st
+import h5py
 import matplotlib.pyplot as plt
 import numpy as np
-import h5py
 import openai
+import requests
+import streamlit as st
 
 from backend.brian import fetch_brian
-
 
 # [MP] this should probably be a script argument
 openai.api_key = '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' 
@@ -43,9 +42,42 @@ response = openai.Completion.create(
 
 loc = response['choices'][0]['text']
 
-st.write(f"I think you are asking about: {loc}")
-# [MP] Sometimes GPT responds 
-city, state, country = loc.split(",")
+## setting default location, when it is not specific
+if ('non-specific' in loc) or ('city is Tomorrow' in loc)\
+    or ('question is too vague' in loc) or ('city is unknown' in loc)\
+    or ('city is Tomorrow' in loc):
+    prompt = f"{original_question} Ignore the previous question. \
+    I set my default location to San Francisco, CA, United States. What state does the above refer to? What country does it refer to? \
+    Respond with city, state, country:"
+
+
+    response = openai.Completion.create(
+        model="text-davinci-002",
+        prompt=prompt,
+        temperature=0,
+        max_tokens=1000,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=[" Human:", " AI:"] #\n
+    )
+
+    loc = response['choices'][0]['text']
+
+
+location = loc.split(",")
+
+city, state, country_code = None, None, None
+if len(location) == 1:
+    city = location[0]
+    st.write(f"I think you are asking about: {city}")
+elif len(location) == 2:
+    city, country_code = location[0], location[1]
+    st.write(f"I think you are asking about: {city}, {country_code}")
+elif len(location) == 3:
+    city, state, country_code = location[0], location[1], location[2]
+    st.write(f"I think you are asking about: {city}, {state}, {country_code}")
+
 
 
 # Get the coordinates 
@@ -60,14 +92,19 @@ def geocode(    city=None,
     Given a city, state, and country, return the latitude and longitude and other data
     """
 
-    url = f"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country_code}&limit={limit}&appid={api_key}"
+    if city and state and country_code:
+        url = f"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country_code}&limit={limit}&appid={api_key}"
+    elif city and country_code: 
+        url = f"http://api.openweathermap.org/geo/1.0/direct?q={city},{country_code}&limit={limit}&appid={api_key}"
+    elif city: 
+        url = f"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit={limit}&appid={api_key}"
     
     # get the response
     response = requests.get(url)
     data = eval(response.text)[0]
     return data
 
-data = geocode(city=city, state=state, country_code=country)
+data = geocode(city=city, state=state, country_code=country_code)
 lat, lon = data['lat'], data['lon']
 
 ########  Part 3: get the weather forecast
